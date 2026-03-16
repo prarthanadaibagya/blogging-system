b2868f917c75bddeac416fc480cea686f774886b
prarthanadaibagya <prarthanadaibagya@gmail.com>
2026-03-16 09:50:58 +0545

    Modification and fixes in blogging CRUD

--- blog/blogg/serializers.py
+++ blog/blogg/serializers.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
 from .models import Post
 
+
 class PostSerializer(serializers.ModelSerializer):
+    author_username = serializers.CharField(source='author.username', read_only=True)
+
     class Meta:
         model = Post
-        fields = '__all__'
-        read_only_fields = ['author']
+        fields = ['id', 'title', 'content', 'author', 'author_username', 'created_at']
+        read_only_fields = ['author', 'author_username', 'created_at']
